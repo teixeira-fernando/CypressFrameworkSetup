e99efe7bbaa43b7bc6043b58433f0ae9e2ca6b75
fernandoteixeira <fernandoadt1@gmail.com>
2019-12-18 17:10:50 -0300

    Configuration for multiple environments

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,12 +11,31 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+const fs = require('fs-extra')
+const path = require('path')
 const cucumber = require('cypress-cucumber-preprocessor').default
 module.exports = (on, config) => {
   on('file:preprocessor', cucumber()),
   on('task', {
      failed: require('cypress-failed-log/src/failed')(),
    }),
-   on('task', require('@cypress/code-coverage/task'))
+  on('task', require('@cypress/code-coverage/task'))
+     //Browser launch
 
+  function processConfigName(on, config) {
+
+    const file = config.env.name || "default"
+    return getConfigFile(file).then(function (file) {
+      //return file object
+      return file;
+    })
+  }
+
+  function getConfigFile(file) {
+    const pathToConfigFile = path.resolve('cypress', 'config', `${file}.json`)
+    return fs.readJson(pathToConfigFile)
+  }
+
+  //Return the configuration file details
+  return processConfigName(on, config);
 }
